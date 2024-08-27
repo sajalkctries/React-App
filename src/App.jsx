39b94e57b6e7b7da85389eddf0406f3d503bf28f
sajalkctries <sajalkc5678@gmail.com>
2024-08-27 13:27:26 +0545

    Added Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import IndexMain from "./IndexMain"
-import "./App.css"
+import Header from "./Header";
+import Footer from "./Footer";
+import IndexMain from "./IndexMain";
+import "./App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      children: [
+        {
+          path: "/",
+          element: <IndexMain slogan={"Unlock Your Potential"} />,
+        },
+        { path: "/home", element: <IndexMain slogan={"Unlock Your Potential"} /> },
+        { path: "/about", element: <div>This is about </div>},
+        { path: "/contact", element: <div>This is Contact </div>},
+      ],
+      element: (
+        <>
+          <Header websiteName={"Squile"} />
+          <Outlet />
+          <Footer />
+        </>
+      ),
+    },
+  ]);
+
   return (
     <div className="app">
-      <Header websiteName={"Squile"}/>
-      <IndexMain slogan={"Intellegence Beyond Imagination"}/>
-      <Footer/>
+      <RouterProvider router={router} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,29 +1,45 @@
-const Header = ({websiteName}) => {
+/* eslint-disable react/prop-types */
+
+import { NavLink } from "react-router-dom";
+
+const Header = ({ websiteName }) => {
   return (
     <>
-    <header>
-      <div className="logo">
-        <a href="index.html">{websiteName}</a>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="login-signup">
-        <button className="log-in">Log in</button>
-        <button className="sign-up">Sign Up</button>
-      </div>
-    </header>
-    <hr />
+      <header>
+        <div className="logo">
+          <NavLink to={"/home"}>{websiteName}</NavLink>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to={"/home"}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/about"}
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/contact"}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="login-signup">
+          <button className="log-in">Log in</button>
+          <button className="sign-up">Sign Up</button>
+        </div>
+      </header>
+      <hr />
     </>
   );
 };
